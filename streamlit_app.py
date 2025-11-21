57289436aa4478c8ee75bc09bd3cf125b1e5d858
Zinedine Amalia <zinedineamalianoor@gmail.com>
2025-11-21 14:18:53 +0700

    Import XGBClassifier from xgboost

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from xgboost import XGBClassifier
 # ================================
 # LOAD ASSETS
 # ================================
